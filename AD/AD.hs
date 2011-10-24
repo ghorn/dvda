f47886fe260438120c94afab21823d9e1d4732e3
Greg Horn <gregmainland@gmail.com>
2011-10-24 11:10:02 -0700

    first commit - simplest forward mode addition/multiplication implemented

---
+++ AD/AD.hs
@@ -0,0 +1,15 @@
+-- AD.hs
+
+{-# OPTIONS_GHC -Wall #-}
+
+data Dual a = Dual a a deriving (Show, Eq)
+
+instance Num a => Num (Dual a) where
+  Dual a b + Dual c d = Dual (a+c) (b+d)
+  Dual a b * Dual c d = Dual (a*c) (a*d + c*b)
+
+diff :: (Show a, Num a) => (Dual a -> Dual a) -> a -> a
+diff f x0 = (\(Dual _ d) -> d) $ f $ Dual x0 1
+
+g :: (Num a) => a -> a
+g x = x*x 
