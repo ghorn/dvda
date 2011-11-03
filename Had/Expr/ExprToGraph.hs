2eb4af821529911a40169c15b6c11f25755a95f7
Greg Horn <gregmainland@gmail.com>
2011-11-03 16:38:08 -0700

    exprsToGraph added to support multiple outputs, graphGobbler now non-monadic

--- Had/Expr/ExprToGraph.hs
+++ Had/Expr/ExprToGraph.hs
@@ -8,7 +8,6 @@ module Had.Expr.ExprToGraph( exprToGraph
                            ) where
 
 import Data.Graph.Inductive hiding (pre, nodes, edges)
-import Control.Monad.State
 import Data.GraphViz
 import Data.Text.Lazy(pack)
 import Data.Maybe
@@ -20,11 +19,12 @@ import Had.Expr.Op2Type
 
 previewGraph :: (Show a, Eq a) => Gr (GraphOp a) (Expr a) -> IO ()
 previewGraph g = preview $ emap show g
-                
+
 data GraphOp a = GSource (SourceType a)
                | GElemwise ElemwiseType
                | GOp2 Op2Type
                | GOutput String
+
 instance (Eq a, Show a) => Show (GraphOp a) where
   show (GSource sourceType) = show sourceType
   show (GElemwise elemwiseType) = show elemwiseType
@@ -36,53 +36,53 @@ instance (Show a, Eq a) => Labellable (GraphOp a) where
   toLabelValue go = toLabelValue $ pack $ show go
 
 exprToGraph :: (Show a, Num a) => Expr a -> Gr (GraphOp a) (Expr a)
-exprToGraph expr = evalState (graphGobbler 0 expr) emptyGP
+exprToGraph expr = exprsToGraph [expr]
+
+exprsToGraph :: (Show a, Num a) => [Expr a] -> Gr (GraphOp a) (Expr a)
+exprsToGraph exprs = foldr addOutput emptyGP labeledExprs
   where
-    emptyGP = mkGraph [(0, GOutput "out")] []
+    labeledExprs = reverse $ zipWith (\k e -> ("out"++show k, e)) [(0::Integer)..] exprs
+    emptyGP = mkGraph [] []
+
+addOutput :: (Show a, Num a) => (String, Expr a) -> Gr (GraphOp a) (Expr a) -> Gr (GraphOp a) (Expr a)
+addOutput (name, expr) graph = graphGobbler newIdx expr newState
+  where
+    newIdx = noNodes graph
+    newState = insNode (newIdx, GOutput name) graph
+
+graphGobbler :: (Show a, Num a) => Int -> Expr a -> Gr (GraphOp a) (Expr a) -> Gr (GraphOp a) (Expr a)
+graphGobbler parentIdx expr oldGraph = ret
+  where
+    existingNode = lookupBy (\(_,_,a) -> a) expr (labEdges oldGraph)
+    newIdx = noNodes oldGraph
+    newGraph
+      | isNothing existingNode = ([], newIdx, getGraphOp expr, [(expr, parentIdx)]) & oldGraph
+      | otherwise = insEdge (idx, parentIdx, expr) oldGraph
+      where
+        (idx,_,_) = (fromJust existingNode)
+  
+    ret = case expr of (Source _)     -> newGraph
+                       (Elemwise _ x) -> graphGobbler newIdx x newGraph
+                       (Op2 _ x y)    -> graphGobbler newIdx y $ graphGobbler newIdx x newGraph
+
+lookupBy :: (Eq a) => (b -> a) -> a -> [b] -> Maybe b
+lookupBy f a bs = lookup a $ map (\x -> (f x, x)) bs
 
 getGraphOp :: (Show a, Eq a) => Expr a -> GraphOp a
 getGraphOp (Source s) = GSource s
 getGraphOp (Elemwise ewt _) = GElemwise ewt
 getGraphOp (Op2 op2 _ _) = GOp2 op2
 
-
-lookupBy :: (Eq a) => (b -> a) -> a -> [b] -> Maybe b
-lookupBy f a bs = lookup a $ map (\x -> (f x, x)) bs
-
-
-graphGobbler :: (Show a, Num a) => Int -> Expr a -> State (Gr (GraphOp a) (Expr a)) (Gr (GraphOp a) (Expr a))
-graphGobbler parentIdx expr = do
-  graph <- get
-
-  let existingNode = lookupBy (\(_,_,a) -> a) expr (labEdges graph)
-      newIdx = noNodes graph
-      newState
-        | isNothing existingNode = ([], newIdx, getGraphOp expr, [(expr, parentIdx)]) & graph
-        | otherwise = insEdge (idx, parentIdx, expr) graph
-        where
-          (idx,_,_) = (fromJust existingNode)
-  
-  put newState
-  
-  case expr of (Source _) -> do
-                 return newState
-               (Elemwise _ x) -> do
-                 graphGobbler newIdx x
-               (Op2 _ x y) -> do
-                 _ <- graphGobbler newIdx x
-                 graphGobbler newIdx y
-
-
 exprToGraphTest :: IO ()
 exprToGraphTest = test
 
 test :: IO ()
 test = do
-  let exampleExpr :: Expr Integer
-      exampleExpr = abs(x*34) + 5 + x
+  let exampleExprs :: [Expr Integer]
+      exampleExprs = [x*34 + 5 + x, 3*x + 3]
         where
           x = sym "x"
-      g = exprToGraph exampleExpr
-  print exampleExpr
+      g = exprsToGraph exampleExprs
+  print exampleExprs
   print g
   previewGraph g
