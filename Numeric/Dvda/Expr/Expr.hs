ce95ebb1ffd6ab315dbb58dd2d58552ce3ec3d7c
Greg Horn <gregmainland@gmail.com>
2011-11-28 20:10:03 -0800

    re-implement data Expr a = EScalar (Scalar a) | EVector (Vector a) | EMatrix (Matrix a), not exporting Matrix/Vector/Scalar value constructors (or type constructors yet)

--- Numeric/Dvda/Expr/Expr.hs
+++ Numeric/Dvda/Expr/Expr.hs
@@ -1,219 +1,255 @@
 -- Expr.hs
 
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE Rank2Types #-}
 
 module Numeric.Dvda.Expr.Expr( Expr(..)
-                             , Dim(..)
---                             , dot
---                             , applyDot
-                             , isNumeric
                              , symbolic
                              , symVec
                              , symMat
                              , vec
                              , mat
-                             , zipBinaryF
                              ) where
 
 --import Data.GraphViz(Labellable(..))
 --import Data.Text.Lazy(pack)
---import qualified Data.Array.Repa as R
 
 import Numeric.Dvda.Expr.BinaryType
 import Numeric.Dvda.Expr.UnaryType
-import Numeric.Dvda.Expr.Scalar
 
-type VecDim = Int
-type MatDim = (Int,Int)
+data Binary a = Binary BinaryType a a deriving (Show, Eq)
+data Unary a = Unary UnaryType a deriving (Show, Eq)
 
-data Dim = Sca
-         | Vec VecDim
-         | Mat MatDim deriving (Show, Eq)
+data Scalar a = SNum a
+              | SSym String
+              | SUnary (Unary (Scalar a))
+              | SBinary (Binary (Scalar a))
+              | SInt Int deriving (Eq, Show)
 
-data Expr a = Sym Dim String
-            | EScalar (Scalar a)
-            | Vector VecDim [a]
-            | Matrix MatDim [a]
-            | Unary UnaryType (Expr a)
-            | Binary BinaryType (Expr a) (Expr a)
-            | Broadcast Dim (Expr a) deriving Eq
+data Vector a = VNum Int [a]
+              | VSym Int String
+              | VUnary (Unary (Vector a))
+              | VBinary (Binary (Vector a))
+              | VBroadcast Int (Scalar a) deriving (Eq, Show)
 
---            | forall sh1 sh2 . (R.Shape sh1, R.Shape sh2, Dottable sh1 sh2 sh) =>
---              Dot { arg1' :: Expr sh1 a
---                  , arg2' :: Expr sh2 a
---                  , dim :: sh
---                  }
+data Matrix a = MNum (Int,Int) [a]
+              | MSym (Int,Int) String
+              | MUnary (Unary (Matrix a))
+              | MBinary (Binary (Matrix a))
+              | MBroadcast (Int,Int) (Scalar a) deriving (Eq, Show)
 
-isNumeric :: Expr a -> Bool
-isNumeric (EScalar (N _)) = True
-isNumeric (Vector _ _) = True
-isNumeric (Matrix _ _) = True
-isNumeric (Broadcast _ x) = isNumeric x
-isNumeric _ = False
-
-isI  :: Int -> Expr a -> Bool
-isI x (EScalar (I y)) = x == y
-isI x (Broadcast _ y) = isI x y
-isI _ _ = False
-
-mapUnary :: Floating a => UnaryType -> Expr a -> Expr a
-mapUnary ut (EScalar (N a)) = EScalar $ N (applyUnary ut a)
-mapUnary ut (Vector d xs) = Vector d $ map (applyUnary ut) xs
-mapUnary ut (Matrix d xs) = Matrix d $ map (applyUnary ut) xs
-mapUnary ut (Broadcast d x) = Broadcast d $ (applyUnary ut x)
-mapUnary ut expr = Unary ut expr
-
-zipBinaryF :: Floating a => BinaryType -> Expr a -> Expr a -> Expr a 
-zipBinaryF binType x' y'
-  | getDim x' == getDim y' = zipBinaryF' x'' y''
-  | otherwise              = error $ unlines [ "type mismatch in zipBinaryF (" ++ show binType ++ ")"
-                                             , "x: " ++ show x'
-                                             , "y: " ++ show y'
-                                             ]
-  where
-    (Binary _ x'' y'') = zipBinary binType x' y' -- call this to prune Add (I 0)/Mul (I 1):
-    
-    zipBinaryF' (EScalar (N x)) (EScalar (N y)) = EScalar (N $ (applyBinary binType) x y)
-    zipBinaryF' (Vector d x) (Vector _ y) = Vector d $ zipWith (applyBinary binType) x y
-    zipBinaryF' (Matrix d x) (Matrix _ y) = Matrix d $ zipWith (applyBinary binType) x y
-    zipBinaryF' x y = Binary binType x y
+--class Exprlike a where
+--  binaryConstructor :: Binary a -> a
+--  unaryConstructor :: Unary a -> a
+--  add :: a -> a -> a
+  
+vecDim :: Vector a -> Int
+vecDim (VNum d _) = d
+vecDim (VSym d _) = d
+vecDim (VBroadcast d _) = d
+vecDim (VUnary (Unary _ v)) = vecDim v
+vecDim (VBinary (Binary _ vx vy)) 
+  | vecDim vx == vecDim vy = vecDim vx
+  | otherwise              = error "vecDim found mismatched dimensions in VBinary - this indicates the absence of proper checking during construction"
 
 
-zipBinary :: Num a => BinaryType -> Expr a -> Expr a -> Expr a 
-zipBinary binType' x' y'
-  | getDim x' == getDim y' = zipBinary' binType' x' y'
-  | otherwise              = error $ unlines [ "type mismatch in zipBinary (" ++ show binType' ++ ")"
-                                             , "x: " ++ show x'
-                                             , "y: " ++ show y'
-                                             ]
-  where
-    zipBinary' Add x y
-      | isI 0 x  = y
-      | isI 0 y  = x
-      | otherwise = Binary Add x y
-    zipBinary' Sub x y
-      | isI 0 x  = negate y
-      | isI 0 y  = x
-      | otherwise = Binary Add x y
-    zipBinary' Mul x y
-      | isI 1 x  = y
-      | isI 1 y  = x
-      | otherwise = Binary Mul x y
-    zipBinary' binType x y = Binary binType x y
-
-getDim :: Expr a -> Dim
-getDim (Sym d _) = d
-getDim (EScalar _) = Sca
-getDim (Vector d _) = Vec d
-getDim (Matrix d _) = Mat d
-getDim (Unary _ arg) = getDim arg
-getDim (Binary _ arg1 _) = getDim arg1
-getDim (Broadcast d _) = d
---getDim (Tensor a) = R.extent a
-
---getDim (Dot { dim = d }) = d
-
-vec :: Num a => [a] -> Expr a
-vec xs = Vector (length xs) xs
-
-mat :: Num a => (Int,Int) -> [a] -> Expr a
-mat (r,c) xs 
-  | length xs == r*c = Matrix (r,c) xs
-  | otherwise        = error $ unlines [ "Improper dimensions in mat :: (Int,Int) -> [a] -> Expr a"
-                                       , "dim: " ++ show (r,c)
-                                       , "[a]: " ++ show xs
-                                       ]
+matDim :: Matrix a -> (Int, Int)
+matDim (MNum d _) = d
+matDim (MSym d _) = d
+matDim (MBroadcast d _) = d
+matDim (MUnary (Unary _ m)) = matDim m
+matDim (MBinary (Binary _ mx my)) 
+  | matDim mx == matDim my = matDim mx
+  | otherwise              = error "matDim found mismatched dimensions in MBinary - this indicates the absence of proper checking during construction"
 
 
-instance Show a => Show (Expr a) where
-  show (Sym _ name) = name
-  show (EScalar x) = show x
-  show (Vector _ v) = show v
-  show (Matrix _ m) = show m
-  show (Unary unaryType arg) = show unaryType ++ "(" ++ show arg ++ ")"
-  show (Binary binaryType arg1 arg2) = "( " ++ show arg1 ++" "++ show binaryType ++" "++ show arg2 ++ " )"
-  show (Broadcast d x) = "broadcast(" ++ show x ++ ", " ++ show d ++ ")"
---  show (Tensor t) = show t
---  show (Dot {arg1' = a1, arg2' = a2}) = "dot( " ++ show a1 ++ ", " ++ show a2 ++ " )"
+
+sIsZero :: Scalar a -> Bool
+sIsZero (SInt 0) = True
+sIsZero _ = False
+
+vIsZero :: Vector a -> Bool
+vIsZero (VBroadcast _ s) = sIsZero s
+vIsZero _ = False
+
+mIsZero :: Matrix a -> Bool
+mIsZero (MBroadcast _ s) = sIsZero s
+mIsZero _ = False
 
 
-broadcast :: Scalar a -> Dim -> Expr a
-broadcast x d = Broadcast d (EScalar x)
+instance Num a => Num (Scalar a) where
+  fromInteger = SInt . fromInteger
+  abs x 
+    | sIsZero x = SInt 0
+    | otherwise = SUnary (Unary Abs x)
+  signum x 
+    | sIsZero x = SInt 0
+    | otherwise = SUnary (Unary Signum x)
+  negate x 
+    | sIsZero x = SInt 0
+    | otherwise = SUnary (Unary Neg x)
+
+  (SInt x) + (SInt y) = SInt $ x + y
+  (SNum x) + (SNum y) = SNum $ x + y
+  (SNum x) + (SInt y) = SNum $ x + (fromIntegral y)
+  (SInt x) + (SNum y) = SNum $ (fromIntegral x) + y
+  x + y
+    | sIsZero x = y
+    | sIsZero y = x
+    | otherwise = SBinary (Binary Add x y)
+  (*) = undefined
+  (-) = undefined
+
+
+instance Num a => Num (Vector a) where
+  fromInteger = error "API problem: fromInteger (in Num a => Num (Vector a)) should not be accessible by the user"
+  abs x 
+    | vIsZero x = VBroadcast (vecDim x) (SInt 0)
+    | otherwise = VUnary (Unary Abs x)
+  signum x 
+    | vIsZero x = VBroadcast (vecDim x) (SInt 0)
+    | otherwise = VUnary (Unary Signum x)
+  negate x 
+    | vIsZero x = VBroadcast (vecDim x) (SInt 0)
+    | otherwise = VUnary (Unary Neg x)
+  (VNum dx xs) + (VNum dy ys)
+    | dx == dy  = VNum dx $ zipWith (+) xs ys
+    | otherwise = error "Dimension mismatch in VNum + VNum"
+  x + y
+    | vIsZero x = y
+    | vIsZero y = x
+    | otherwise = VBinary (Binary Add x y)
+  (*) = undefined
+  (-) = undefined
+
+
+instance Num a => Num (Matrix a) where
+  fromInteger = error "API problem: fromInteger (in Num a => Num (Matrix a)) should not be accessible by the user"
+  abs x 
+    | mIsZero x = MBroadcast (matDim x) (SInt 0)
+    | otherwise = MUnary (Unary Abs x)
+  signum x 
+    | mIsZero x = MBroadcast (matDim x) (SInt 0)
+    | otherwise = MUnary (Unary Signum x)
+  negate x 
+    | mIsZero x = MBroadcast (matDim x) (SInt 0)
+    | otherwise = MUnary (Unary Neg x)
+  (MNum dx xs) + (MNum dy ys)
+    | dx == dy  = MNum dx $ zipWith (+) xs ys
+    | otherwise = error "Dimension mismatch in MNum + MNum"
+  x + y
+    | mIsZero x = y
+    | mIsZero y = x
+    | otherwise = MBinary (Binary Add x y)
+  (*) = undefined
+  (-) = undefined
+
+
+safeBinaryConstruct :: Num a => (forall b . Num b => b -> b -> b) -> Expr a -> Expr a -> Expr a
+-- normal combination:
+safeBinaryConstruct f (EScalar x) (EScalar y) = EScalar $ f x y
+safeBinaryConstruct f (EVector x) (EVector y) = EVector $ f x y
+safeBinaryConstruct f (EMatrix x) (EMatrix y) = EMatrix $ f x y
+-- broadcast scalar to vector:
+safeBinaryConstruct f (EScalar x) (EVector y) = EVector $ f (VBroadcast (vecDim y) x) y
+safeBinaryConstruct f (EVector x) (EScalar y) = EVector $ f x (VBroadcast (vecDim x) y)
+-- broadcast scalar to matrix:
+safeBinaryConstruct f (EScalar x) (EMatrix y) = EMatrix $ f (MBroadcast (matDim y) x) y
+safeBinaryConstruct f (EMatrix x) (EScalar y) = EMatrix $ f x (MBroadcast (matDim x) y)
+-- illegal combination:
+safeBinaryConstruct _ (EVector _) (EMatrix _) = error "safeBinaryConstruct error: Can't combine vector with matrix"
+safeBinaryConstruct _ (EMatrix _) (EVector _) = error "safeBinaryConstruct error: Can't combine vector with matrix"
+
+
+safeUnaryConstruct :: Num a => (forall b . Num b => b -> b) -> Expr a -> Expr a
+safeUnaryConstruct f (EScalar x) = EScalar $ f x
+safeUnaryConstruct f (EVector x) = EVector $ f x
+safeUnaryConstruct f (EMatrix x) = EMatrix $ f x
 
 instance Num a => Num (Expr a) where
-  -- (+)
-  (EScalar x) + (EScalar y) = EScalar (x + y)
-  (EScalar x) + y = (broadcast x (getDim y)) + y
-  x + (EScalar y) = x + (broadcast y (getDim x))
-  x + y = zipBinary Add x y
+  x + y = safeBinaryConstruct (+) x y
+  x * y = safeBinaryConstruct (*) x y
+  x - y = safeBinaryConstruct (-) x y
+  abs = safeUnaryConstruct abs
+  signum = safeUnaryConstruct abs
+  fromInteger i = EScalar (SInt (fromInteger i))
   
-  -- (*)
-  (EScalar x) * (EScalar y) = EScalar (x * y)
-  (EScalar x) * y = (broadcast x (getDim y)) * y
-  x * (EScalar y) = x * (broadcast y (getDim x))
-  x * y = zipBinary Mul x y
 
-  -- (-)
-  (EScalar x) - (EScalar y) = EScalar (x - y)
-  (EScalar x) - y = (broadcast x (getDim y)) - y
-  x - (EScalar y) = x - (broadcast y (getDim x))
-  x - y = zipBinary Sub x y
-
-  negate x = Unary Neg x
-  abs x = Unary Abs x
-  signum x = Unary Signum x
-  fromInteger x = EScalar $ I (fromInteger x)
+data Expr a = EScalar (Scalar a)
+            | EVector (Vector a)
+            | EMatrix (Matrix a) deriving (Eq, Show)
 
 
+symbolic :: String -> Expr a
+symbolic name = EScalar $ SSym name
 
-instance Fractional a => Fractional (Expr a) where
-  (EScalar x) / (EScalar y) = EScalar (x / y)
-  (EScalar x) / y = (broadcast x (getDim y)) / y
-  x / (EScalar y) = x / (broadcast y (getDim x))
-  x / y = zipBinary Div x y
+symVec :: String -> Int -> Expr a
+symVec name d = EVector $ VSym d name
 
-  fromRational x = EScalar $ fromRational x
+symMat :: String -> (Int,Int) -> Expr a
+symMat name d = EMatrix $ MSym d name
+
+vec :: [a] -> Expr a
+vec xs = EVector $ VNum (length xs) xs
+
+mat :: (Int,Int) -> [a] -> Expr a
+mat (r,c) xs 
+  | length xs == r*c = EMatrix $ MNum (r,c) xs
+  | otherwise        = error "Improper dimensions in mat :: (Int,Int) -> [a] -> Expr a"
+
+--instance Show a => Show (Expr a) where
+--  show (Sym _ name) = name
+--  show (EScalar x) = show x
+--  show (Vector _ v) = show v
+--  show (Matrix _ m) = show m
+--  show (Unary unaryType arg) = show unaryType ++ "(" ++ show arg ++ ")"
+--  show (Binary binaryType arg1 arg2) = "( " ++ show arg1 ++" "++ show binaryType ++" "++ show arg2 ++ " )"
+--  show (Broadcast d x) = "broadcast(" ++ show x ++ ", " ++ show d ++ ")"
+----  show (Tensor t) = show t
+----  show (Dot {arg1' = a1, arg2' = a2}) = "dot( " ++ show a1 ++ ", " ++ show a2 ++ " )"
+--
+--
+--
+--instance Fractional a => Fractional (Expr a) where
+--  (EScalar x) / (EScalar y) = EScalar (x / y)
+--  (EScalar x) / y = (broadcast x (getDim y)) / y
+--  x / (EScalar y) = x / (broadcast y (getDim x))
+--  x / y = zipBinary Div x y
+--
+--  fromRational x = EScalar $ fromRational x
 --  fromRational x = num / den
 --    where
 --      num = fromIntegral $ numerator x
 --      den = fromIntegral $ denominator x
-
-
-instance (Floating a) => Floating (Expr a) where
-  pi = EScalar $ N pi
-  
-  exp x  = mapUnary Exp x
-  sqrt x = mapUnary Sqrt x
-  log x  = mapUnary Log x
-  
-  x**y = zipBinary Pow x y
-  logBase x y = zipBinary Pow x y
-  
-  sin x = mapUnary Sin x
-  cos x = mapUnary Cos x
-  tan x = mapUnary Tan x
-                   
-  asin x = mapUnary ASin x
-  acos x = mapUnary ACos x
-  atan x = mapUnary ATan x
-
-  sinh x = mapUnary Sinh x
-  cosh x = mapUnary Cosh x
-  tanh x = mapUnary Tanh x
-
-  asinh x = mapUnary ASinh x
-  acosh x = mapUnary ACosh x
-  atanh x = mapUnary ATanh x
-
-
---instance (Show a, Num a, R.Elt a, Show sh, R.Shape sh) => Labellable (Expr sh a) where
---  toLabelValue go = toLabelValue $ pack $ show go ++ "["++show (getDim go)++"]"
-
-symbolic :: String -> Expr a
-symbolic name = Sym Sca name
-
-symVec :: String -> Int -> Expr a
-symVec name d = Sym (Vec d) name
-
-symMat :: String -> (Int,Int) -> Expr a
-symMat name d = Sym (Mat d) name
+--
+--
+--instance (Floating a) => Floating (Expr a) where
+--  pi = EScalar $ N pi
+--  
+--  exp x  = mapUnary Exp x
+--  sqrt x = mapUnary Sqrt x
+--  log x  = mapUnary Log x
+--  
+--  x**y = zipBinary Pow x y
+--  logBase x y = zipBinary Pow x y
+--  
+--  sin x = mapUnary Sin x
+--  cos x = mapUnary Cos x
+--  tan x = mapUnary Tan x
+--                   
+--  asin x = mapUnary ASin x
+--  acos x = mapUnary ACos x
+--  atan x = mapUnary ATan x
+--
+--  sinh x = mapUnary Sinh x
+--  cosh x = mapUnary Cosh x
+--  tanh x = mapUnary Tanh x
+--
+--  asinh x = mapUnary ASinh x
+--  acosh x = mapUnary ACosh x
+--  atanh x = mapUnary ATanh x
+--
+--
+----instance (Show a, Num a, R.Elt a, Show sh, R.Shape sh) => Labellable (Expr sh a) where
+----  toLabelValue go = toLabelValue $ pack $ show go ++ "["++show (getDim go)++"]"
+--
