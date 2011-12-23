65b9623f1a53902bde10f7f39b02bc3edc8e2cd1
Greg Horn <gregmainland@gmail.com>
2011-12-22 18:13:56 -0800

    rename safeConstructUnary --> tmap

--- Numeric/Dvda/Internal/Expr.hs
+++ Numeric/Dvda/Internal/Expr.hs
@@ -3,17 +3,24 @@
    Description : Basic symbolic expression type
 
    This module instances 'Numeric.Dvda.Expr.Expr` as `Prelude.Num`\/`Prelude.Fractional`\/`Prelude.Floating`.
+   .
+   Expr exists as an extraction barrier for Tensor. Expr wraps Tensor and the user only ever sees/uses Expr. This lets us do all dimensionality checking on Expr and when working with Tensor we can assume that dimensions are correct.
  -}
 
 {-# OPTIONS_GHC -Wall #-}
 
 module Numeric.Dvda.Internal.Expr( Expr(..)
+                                 , tmap
                                  ) where
 
 import Numeric.Dvda.Internal.Tensor
 import Numeric.Dvda.Dim
 
 data Expr a = Expr (Tensor a) deriving Eq
+
+-- like fmap but only for Expr (Tensor a)
+tmap :: Num a => (Tensor a -> Tensor a) -> Expr a -> Expr a
+tmap f (Expr x) = Expr $ f x
 
 instance Show a => Show (Expr a) where
   show (Expr x) = show x
@@ -22,8 +29,8 @@ instance Num a => Num (Expr a) where
   x + y = safeBinaryConstruct (+) x y
   x * y = safeBinaryConstruct (*) x y
   x - y = safeBinaryConstruct (-) x y
-  abs = safeUnaryConstruct abs
-  signum = safeUnaryConstruct signum
+  abs = tmap abs
+  signum = tmap signum
   fromInteger i = Expr (TInt D0 [fromInteger i])
 
 instance Fractional a => Fractional (Expr a) where
@@ -33,31 +40,28 @@ instance Fractional a => Fractional (Expr a) where
 instance (Floating a) => Floating (Expr a) where
   pi = Expr $ TNum D0 [pi]
   
-  exp  = safeUnaryConstruct exp
-  sqrt = safeUnaryConstruct sqrt
-  log  = safeUnaryConstruct log
+  exp  = tmap exp
+  sqrt = tmap sqrt
+  log  = tmap log
   
   (**) = safeBinaryConstruct (**)
   logBase = safeBinaryConstruct logBase
   
-  sin = safeUnaryConstruct sin
-  cos = safeUnaryConstruct cos
-  tan = safeUnaryConstruct tan
+  sin = tmap sin
+  cos = tmap cos
+  tan = tmap tan
                    
-  asin = safeUnaryConstruct asin
-  acos = safeUnaryConstruct acos
-  atan = safeUnaryConstruct atan
+  asin = tmap asin
+  acos = tmap acos
+  atan = tmap atan
 
-  sinh = safeUnaryConstruct sinh
-  cosh = safeUnaryConstruct cosh
-  tanh = safeUnaryConstruct tanh
+  sinh = tmap sinh
+  cosh = tmap cosh
+  tanh = tmap tanh
 
-  asinh = safeUnaryConstruct asinh
-  acosh = safeUnaryConstruct acosh
-  atanh = safeUnaryConstruct atanh
-
-safeUnaryConstruct :: Num a => (Tensor a -> Tensor a) -> Expr a -> Expr a
-safeUnaryConstruct f (Expr x) = Expr $ f x
+  asinh = tmap asinh
+  acosh = tmap acosh
+  atanh = tmap atanh
 
 -- | all vector/vector and matrix/matrix dimension checking is done here, not in Num instances of Tensor
 --   this is because those Num instances aren't exported and are only called through the Expr api
