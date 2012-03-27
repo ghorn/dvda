246b4e13a930b65f68739166ac0e349bb91f6f62
Greg Horn <gregmainland@gmail.com>
2012-03-26 22:11:27 -0700

    add symMat/symVec

--- Numeric/Dvda/Symbolic/SymMonad.hs
+++ Numeric/Dvda/Symbolic/SymMonad.hs
@@ -17,20 +17,20 @@ type Key = Int
 
 data FunGraph a = FunGraph [(Key, Expr a)] deriving (Show, Eq)
 
-freeKey :: FunGraph a -> Key
-freeKey (FunGraph xs) = length xs
-
-insert :: Key -> Expr a -> FunGraph a -> FunGraph a
-insert k el (FunGraph xs) = FunGraph $ xs ++ [(k, el)]
-
 sym :: String -> State (FunGraph a) (Expr a)
 sym = node . (ESym D0)
 
+symVec :: Int -> String -> State (FunGraph a) (Expr a)
+symVec d = node . (ESym (Dim [d]))
+
+symMat :: (Int,Int) -> String -> State (FunGraph a) (Expr a)
+symMat (r,c) = node . (ESym (Dim [r,c]))
+
 node :: Expr a -> State (FunGraph a) (Expr a)
 node expr = do
-  gr <- get
-  let k = freeKey gr
-  put (insert k expr gr)
+  (FunGraph xs) <- get
+  let k = length xs
+  put (FunGraph $ xs ++ [(k,expr)])
   return (ERef (dim expr) k)
 
 makeFun :: State (FunGraph a) b -> FunGraph a
