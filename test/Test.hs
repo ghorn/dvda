f32d7d0a3b6296b3d687ad2bac201d632557f29b
Matthew Peddie <peddie@alum.mit.edu>
2012-07-16 13:35:38 -0700

    fix domain for sqrt in test code

--- test/Test.hs
+++ test/Test.hs
@@ -97,7 +97,7 @@ unary = [abs, negate, signum, exp, sqrt, log, sin, cos, tan, asin, acos, atan, t
 -- We have to do this by hand because of all kinds of stupid type
 -- shit.  Otherwise we'd map over unary.
 
-cosProp, sinProp, tanProp, coshProp, sinhProp, tanhProp, expProp, signumProp, sqrtProp, negateProp, absProp :: (Eq a, Floating a, Num (Vector a), Show a, Hashable a, Container Vector a) => a -> Bool
+cosProp, sinProp, tanProp, coshProp, sinhProp, tanhProp, expProp, signumProp, negateProp, absProp :: (Eq a, Floating a, Num (Vector a), Show a, Hashable a, Container Vector a) => a -> Bool
 cosProp x = cos x == nativeRun cos x
 sinProp x = sin x == nativeRun sin x
 tanProp x = tan x == nativeRun tan x
@@ -106,31 +106,31 @@ sinhProp x = sinh x == nativeRun sinh x
 tanhProp x = tanh x == nativeRun tanh x
 expProp x = exp x == nativeRun exp x
 signumProp x = signum x == nativeRun signum x
-sqrtProp x = sqrt x == nativeRun sqrt x
 negateProp x = negate x == nativeRun negate x
 absProp x = abs x == nativeRun abs x
 
 props :: [Double -> Bool]
 props = [ cosProp, sinProp, tanProp
         , coshProp, sinhProp, tanhProp
-        , expProp, signumProp, sqrtProp
+        , expProp, signumProp
         , negateProp, absProp
         ]
 
 propNames :: [String]
-propNames = [ "cos", "sin", "tan", "cosh", "sinh", "tanh", "exp", "signum", "sqrt", "negate", "abs"]
+propNames = [ "cos", "sin", "tan", "cosh", "sinh", "tanh", "exp", "signum", "negate", "abs"]
 
-acosProp, asinProp, atanProp :: (Floating a, Num (Vector a), Ord a, Show a, Hashable a, Container Vector a) => a -> Property
+acosProp, asinProp, atanProp, sqrtProp :: (Floating a, Num (Vector a), Ord a, Show a, Hashable a, Container Vector a) => a -> Property
 
-acosProp x = x >= 0 && x <= 1 ==> acos x == nativeRun acos x
-asinProp x = x >= 0 && x <= 1 ==> asin x == nativeRun asin x
-atanProp x = x >= 0 && x <= 1 ==> atan x == nativeRun atan x
+acosProp x = (x >= 0 && x <= 1) ==> abs (acos x - nativeRun acos x) < 1e-12
+asinProp x = (x >= 0 && x <= 1) ==> asin x == nativeRun asin x
+atanProp x = (x >= 0 && x <= 1) ==> atan x == nativeRun atan x
+sqrtProp x = (x >= 0) ==> abs ((sqrt x) - (nativeRun sqrt x)) < 1e-13
 
 prop'Names :: [String]
-prop'Names = ["acos", "asin", "atan"]
+prop'Names = ["acos", "asin", "atan", "sqrt"]
 
 props' :: [Double -> Property]
-props' = [ acosProp, asinProp, atanProp ]
+props' = [ acosProp, asinProp, atanProp, sqrtProp ]
 
 mkUnaryTest (n, t) = testProperty ("unary_" ++ n) t
 
