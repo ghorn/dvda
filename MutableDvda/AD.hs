616ef8dcf5c2b98d8b450619971b6005626b0b7a
Greg Horn <gregmainland@gmail.com>
2012-07-26 17:47:18 +0200

    renamed rad --> backprop, new rad function :: Expr a -> [Expr a] -> IO [Expr a]

--- MutableDvda/AD.hs
+++ MutableDvda/AD.hs
@@ -22,8 +22,8 @@ bpBinary :: (Eq a, Num a)
 bpBinary sens g h binop = do
    let dfdg = dualPerturbation $ binop (Dual g 1) (Dual h 0)
        dfdh = dualPerturbation $ binop (Dual g 0) (Dual h 1)
-   gsens <- backprop (sens*dfdg) g
-   hsens <- backprop (sens*dfdh) h
+   gsens <- backpropNode (sens*dfdg) g
+   hsens <- backpropNode (sens*dfdh) h
    return $ gsens ++ hsens
 
 bpUnary :: (Eq a, Num a)
@@ -32,39 +32,44 @@ bpUnary :: (Eq a, Num a)
            -> IO [(Expr a, Expr a)]
 bpUnary sens g unop = do
    let dfdg = dualPerturbation $ unop (Dual g 1)
-   backprop (sens*dfdg) g
+   backpropNode (sens*dfdg) g
 
-backprop :: Eq a => Expr a -> Expr a -> IO [(Expr a, Expr a)]
-backprop sens e@(ERef _) = readExpr e >>= backprop sens
-backprop sens e@(ESym _) = return [(e,sens)]
-backprop _ (EConst _) = return []
-backprop _ (ENum (FromInteger _)) = return []
-backprop _ (EFractional (FromRational _)) = return []
-backprop sens (ENum (Mul x y)) = bpBinary sens x y (*)
-backprop sens (ENum (Add x y)) = bpBinary sens x y (+)
-backprop sens (ENum (Sub x y)) = bpBinary sens x y (-)
-backprop sens (ENum (Abs x))    = bpUnary sens x abs
-backprop sens (ENum (Negate x)) = bpUnary sens x negate
-backprop sens (ENum (Signum x)) = bpUnary sens x signum
-backprop sens (EFractional (Div x y)) = bpBinary sens x y (/)
-backprop sens (EFloating (Pow x y)) = bpBinary sens x y (**)
-backprop sens (EFloating (LogBase x y)) = bpBinary sens x y logBase
-backprop sens (EFloating (Exp x))   = bpUnary sens x exp
-backprop sens (EFloating (Log x))   = bpUnary sens x log
-backprop sens (EFloating (Sin x))   = bpUnary sens x sin
-backprop sens (EFloating (Cos x))   = bpUnary sens x cos
-backprop sens (EFloating (ASin x))  = bpUnary sens x asin
-backprop sens (EFloating (ATan x))  = bpUnary sens x atan
-backprop sens (EFloating (ACos x))  = bpUnary sens x acos
-backprop sens (EFloating (Sinh x))  = bpUnary sens x sinh
-backprop sens (EFloating (Cosh x))  = bpUnary sens x cosh
-backprop sens (EFloating (Tanh x))  = bpUnary sens x tanh
-backprop sens (EFloating (ASinh x)) = bpUnary sens x asinh
-backprop sens (EFloating (ATanh x)) = bpUnary sens x atanh
-backprop sens (EFloating (ACosh x)) = bpUnary sens x acosh
-backprop sens (EGraphRef x _)       = backprop sens x
+backpropNode :: Eq a => Expr a -> Expr a -> IO [(Expr a, Expr a)]
+backpropNode sens e@(ERef _) = readExpr e >>= backpropNode sens
+backpropNode sens e@(ESym _) = return [(e,sens)]
+backpropNode _ (EConst _) = return []
+backpropNode _ (ENum (FromInteger _)) = return []
+backpropNode _ (EFractional (FromRational _)) = return []
+backpropNode sens (ENum (Mul x y)) = bpBinary sens x y (*)
+backpropNode sens (ENum (Add x y)) = bpBinary sens x y (+)
+backpropNode sens (ENum (Sub x y)) = bpBinary sens x y (-)
+backpropNode sens (ENum (Abs x))    = bpUnary sens x abs
+backpropNode sens (ENum (Negate x)) = bpUnary sens x negate
+backpropNode sens (ENum (Signum x)) = bpUnary sens x signum
+backpropNode sens (EFractional (Div x y)) = bpBinary sens x y (/)
+backpropNode sens (EFloating (Pow x y)) = bpBinary sens x y (**)
+backpropNode sens (EFloating (LogBase x y)) = bpBinary sens x y logBase
+backpropNode sens (EFloating (Exp x))   = bpUnary sens x exp
+backpropNode sens (EFloating (Log x))   = bpUnary sens x log
+backpropNode sens (EFloating (Sin x))   = bpUnary sens x sin
+backpropNode sens (EFloating (Cos x))   = bpUnary sens x cos
+backpropNode sens (EFloating (ASin x))  = bpUnary sens x asin
+backpropNode sens (EFloating (ATan x))  = bpUnary sens x atan
+backpropNode sens (EFloating (ACos x))  = bpUnary sens x acos
+backpropNode sens (EFloating (Sinh x))  = bpUnary sens x sinh
+backpropNode sens (EFloating (Cosh x))  = bpUnary sens x cosh
+backpropNode sens (EFloating (Tanh x))  = bpUnary sens x tanh
+backpropNode sens (EFloating (ASinh x)) = bpUnary sens x asinh
+backpropNode sens (EFloating (ATanh x)) = bpUnary sens x atanh
+backpropNode sens (EFloating (ACosh x)) = bpUnary sens x acosh
+backpropNode sens (EGraphRef x _)       = backpropNode sens x
 
-rad :: (Num a, Eq a, Hashable a) => Expr a -> IO (HashMap (Expr a) (Expr a))
-rad x = do
-  sensitivities <- backprop 1 x
+backprop :: (Num a, Eq a, Hashable a) => Expr a -> IO (HashMap (Expr a) (Expr a))
+backprop x = do
+  sensitivities <- backpropNode 1 x
   return $ HM.fromListWith (+) sensitivities
+
+rad :: (Num a, Eq a, Hashable a) => Expr a -> [Expr a] -> IO [Expr a]
+rad x args = do
+  sensitivities <- backprop x
+  return $ map (\arg -> HM.lookupDefault 0 arg sensitivities) args
