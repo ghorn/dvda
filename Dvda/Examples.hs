2417f3dd8d6f2f2dd0acc89bed7f27548a8e1dc7
Greg Horn <gregmainland@gmail.com>
2012-08-09 13:04:51 +0200

    updated documentation and examples and made version bump for hackage release

--- Dvda/Examples.hs
+++ Dvda/Examples.hs
@@ -1,69 +1,54 @@
 {-# OPTIONS_GHC -Wall #-}
 
-module Dvda.Examples ( run
-                     , run1
-                     , dotVis
+module Dvda.Examples ( doCse
+                     , showFg
+                     , cgen
+                     , mexgen
                      ) where
 
 import Dvda.Expr
 import Dvda.FunGraph
 import Dvda.CGen
-import Dvda.Vis
+import Dvda.Vis ( previewGraph )
+import Dvda.CSE ( cse )
+import Dvda.AD ( rad )
 
-run :: IO ()
-run = do
-  let x = sym "x" :: Expr Double
-      y = sym "y"
-      z = sym "z"
-      w = sym "w"
-      w1 = sym "w1"
-      w2 = sym "w2"
-      w3 = sym "w3"
-      f0 = x*y + z + w1 + w2
-      f2 = f0 * w2/w3
-      
-      f1 = [f0/2, f0*y, w, 0.0, 0]
-      boo = x
+-- a random function to use in different examples
+someFunGraph :: IO (FunGraph Double)
+someFunGraph = toFunGraph inputs outputs
+  where
+    x = sym "x" :: Expr Double
+    y = sym "y"
+    z = sym "z"
+    w = sym "w"
+    w1 = sym "w1"
+    w2 = sym "w2"
+    w3 = sym "w3"
+    f0 = x*y + z + w1 + w2
+    f2 = f0 * w2/w3
+    
+    f1 = [f0/2, f0*y, w, 0.0, 0]
+    boo = x
 
-      inputs = boo :* [y]:*[[z]] :* [w3,w1,w2,w]
-      outputs = f0:*f1:*f2:*[[f0*f0]]
+    inputs = boo :* [y]:*[[z]] :* [w3,w1,w2,w]
+    outputs = f0:*f1:*f2:*[[f0*f0]]:*(rad f2 [x,y,z,w,w1,w2,w3])
 
---  showC "foo" inputs outputs >>= putStrLn
+-- | do cse on a fungraph and count nodes
+doCse :: IO ()
+doCse = do
+  fg' <- someFunGraph
+  putStrLn $ "fungraph has " ++ show (countNodes fg') ++ " nodes"
+  let fg = cse fg'
+  putStrLn $ "fungraph has " ++ show (countNodes fg) ++ " nodes after cse"
 
-  fg' <- toFunGraph inputs outputs
-  putStrLn $ "cost has " ++ show (countNodes fg') ++ " nodes"
-  fg <- toFunGraph inputs outputs
-  putStrLn $ "cost has " ++ show (countNodes fg) ++ " nodes"
-  previewGraph fg
-  putStrLn $ showMex "foo" fg
+-- | show a fungraph
+showFg :: IO ()
+showFg = someFunGraph >>= previewGraph
 
-run1 :: IO ()
-run1 = do
-  let a = sym "a" :: Expr Double
-      b = sym "b"
-      c = sym "c"
-      d = sym "d"
-      e = sym "e"
-      f = sym "f"
+-- | c code generation
+cgen :: IO ()
+cgen = fmap (showC RowMajor "foo") someFunGraph >>= putStrLn
 
-      inputs = [[a,b,c],[d,e,f]]
-      outputs = [[a,b,c],[d,e,f]]
-
-  fg <- toFunGraph inputs outputs
-  putStrLn $ showC RowMajor "foo" fg
-  putStrLn $ showMex "foo" fg
-
-dotVis :: IO ()
-dotVis = do
-  let x = sym "x" :: Expr Double
-      y = sym "y"
-      z = sym "z"
-
-      a0 = x + y + z
-      a1 = x*a0/z
-      a2 = a1 + a0/y
-
-      inputs = x :* [y] :* [[z]]
-      outputs = x*x :* a1 :* a0 :* [[a2]]
-  fg <- toFunGraph inputs outputs
-  previewGraph fg
+-- | mex function generation
+mexgen :: IO ()
+mexgen = fmap (showMex "foo") someFunGraph >>= putStrLn
