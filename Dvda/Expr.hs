dcfc50fb83a2e55fdc6a42d00ebcad63b5e54ced
Greg Horn <gregmainland@gmail.com>
2012-08-10 19:47:57 +0200

    export fromNeg

--- Dvda/Expr.hs
+++ Dvda/Expr.hs
@@ -24,6 +24,7 @@ module Dvda.Expr ( Expr(..)
                  , substitute
                  , sketchySubstitute
                  , foldExpr
+                 , fromNeg
                  ) where
 
 import Control.Applicative ( (<$>), (<*>), pure )
