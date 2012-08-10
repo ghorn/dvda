79f06d4c82439e1b18d41ff6839e51691b2e313b
Greg Horn <gregmainland@gmail.com>
2012-08-10 19:45:00 +0200

    version bump, and export cse from Dvda module

--- Dvda.hs
+++ Dvda.hs
@@ -17,6 +17,7 @@ module Dvda ( -- * primitives
             , Expr
               -- * construct FunGraphs
             , toFunGraph
+            , cse
               -- * show/summarize FunGraphs
             , previewGraph
             , previewGraph'
@@ -29,6 +30,7 @@ module Dvda ( -- * primitives
             ) where
 
 import Dvda.AD ( rad )
+import Dvda.CSE ( cse )
 import Dvda.Expr ( Expr, sym, symDependent, symDependentN )
 import Dvda.FunGraph ( toFunGraph, (:*)(..) )
 import Dvda.Vis ( previewGraph, previewGraph' )
